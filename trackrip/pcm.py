4a5fa7151943ebeb5b52d4d986e8b4e258297ee5
Daniel Brookman <dannntrax@gmail.com>
2020-10-15 19:17:52 -0400

    Correct signed_to_unsigned_8bit explaination

--- trackrip/pcm.py
+++ trackrip/pcm.py
@@ -10,10 +10,10 @@ def signed_to_unsigned_8bit(data):
     # as the unsigned value 255. -1 is halfway in the signed range (-127 to 127)
     # at position 127 (counting the sign bit). However, 255 corresponds to the
     # very end of the unsigned range. To match the original position as a signed
-    # number, we subtract 128, landing us at the original position at 127.
+    # number, we subtract 128, landing us at the original position of 127.
     # While positive decimals will be the same signed or unsigned (under 127),
     # their relative position will change again. 127 is position 255 while
-    # signed, and 128 while unsigned. We add 128, and arrive at 255, the correct
+    # signed, and 127 while unsigned. We add 128 and arrive at 255, the correct
     # position.
     converted = bytearray()
     for byte in data:
